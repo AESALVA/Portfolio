a393ca7ad51bf79da08bebbe3699864196b9c0b8
AESalva <eduardo_salva@hotmail.com>
2023-03-24 00:22:41 -0300

    add

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Container from "react-bootstrap/Container";
+import "../styles/about.css"
+
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <Container className='AboutContainer'>
+      <h2>About me</h2>
+
+
+    </Container>
   )
 }
 
