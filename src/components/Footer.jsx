d18f1f69c781d8e3cd879b9437bc216a5a89b752
AESalva <eduardo_salva@hotmail.com>
2023-03-27 21:31:46 -0300

    add new features to footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "../styles/footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faInstagram, faGithub, faSpotify } from '@fortawesome/free-brands-svg-icons'
+import Nav from "react-bootstrap/Nav";
 
 
 const Footer = () => {
   return (
     <>
       <div className="Footer mt-auto">
-        <div className="ContainerLogo">
+        <div className="ContainerLogo Icon">
           <img src="public/Logo.jpg" alt="logo" />
         </div>
         <div className="SocialIcons">
-          <FontAwesomeIcon icon={faFacebook} />
-          <FontAwesomeIcon icon={faInstagram} />
-          <FontAwesomeIcon icon={faGithub} />
-          <FontAwesomeIcon icon={faSpotify} />
+          <div className="Icon"><Nav.Link className="d-flex"><FontAwesomeIcon icon={faFacebook} /></Nav.Link></div>
+          <div className="Icon"><Nav.Link className="d-flex"><FontAwesomeIcon icon={faInstagram} /></Nav.Link></div>
+          <div className="Icon"><Nav.Link className="d-flex"><FontAwesomeIcon icon={faGithub} /></Nav.Link></div>
+          <div className="Icon"><Nav.Link className="d-flex"><FontAwesomeIcon icon={faSpotify} /></Nav.Link></div>
         </div>
         <p>Copyright © 2023 Angel Eduardo Salva</p>
       </div>
