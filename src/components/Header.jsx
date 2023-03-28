d96c5fd9db1b8f5a5f86ed8b66f28369ab2a0089
AESalva <eduardo_salva@hotmail.com>
2023-03-28 20:13:37 -0300

    new features on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ const Header = () => {
   return (
     <div className="HeaderContainer">
       <div className="HeaderPic">
-      <div className="CristalCard">
-        <h3>My name is Eduardo Salva</h3>
-        <p>and i'm Web Full Stack developer</p>
-      </div>
+        <div className="CristalCard">
+          <div className="CristalCardContainer">
+          <div className="ContainerPicture">
+              <img src="src/assets/eduardoprofile.png" alt="profilepicture" />
+            </div>
+            <div className="TextProfile">
+            <h2>My name is Eduardo Salva</h2>
+            <h3>and i'm Web Full Stack developer</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
