b30ad4ed630763e17c36ae0def08e3fb2ad4d90d
AESalva <eduardo_salva@hotmail.com>
2023-03-29 19:31:55 -0300

    add new animation to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/header.css";
 
 const Header = () => {
+
+const [style, setStyle] = useState('TextProfile');
+const [styleTwo, setStyleTwo] = useState('');
+
+  useEffect(() => {
+    setStyle('TextProfileOn');
+    setStyleTwo('TextProfileBackOn');
+  }, [])
+  
   return (
     <div className="HeaderContainer">
       <div className="HeaderPic">
         <div className="CristalCard">
           <div className="CristalCardContainer">
-            <div className="TextProfileBack">
-              <div className="TextProfile">
+            <div className={`TextProfileBack ${styleTwo}`}>
+              <div className={style}>
               <div className="ContainerPicture">
                   <img
                     src="src/assets/eduardoprofile.png"
@@ -18,7 +27,6 @@ const Header = () => {
                 <div className="Text">
                 <h2>My name is Eduardo Salva</h2>
                 <h3>and i'm Web Full Stack developer</h3></div>
-                {/* <img src="src/assets/eduardoprofile.png" alt="profilepicture" /> */}
               </div>
             </div>
           </div>
