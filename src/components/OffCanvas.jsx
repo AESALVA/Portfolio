69ecf464ff805b3398245cc4c11a5c5b7100eeb7
AESalva <eduardo_salva@hotmail.com>
2023-03-30 18:21:34 -0300

    wsasd

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -21,14 +21,14 @@ const OffCanvas = () => {
           icon={faBarsStaggered}
         />
       </div>
-      <Offcanvas className="OffCanvas bg-dark" show={show} placement={'top'} backdrop={'static'}  name={'top'}>
+      <Offcanvas className="OffCanvas bg-dark" show={show} placement={'top'} onHide={handleClose} name={'top'}>
         <Offcanvas.Header className="px-3 pb-0" closeButton>
             <OffcanvasTitle className="d-flex Color"><div className='ContainerImg'><img src='Logo.jpg' alt='logo' ></img></div>A.E.S</OffcanvasTitle>
         </Offcanvas.Header>
         <Offcanvas.Body className="p-2 d-flex align-items-center pt-0">
     <ul className="NavListResponsive ">
         <li><Nav.Link onClick={handleClose}  href="https://aesalva.github.io/portfolio/"><FontAwesomeIcon icon={faHouse} />{" "}Home</Nav.Link></li>
-        <li><Nav.Link href="#About" ><FontAwesomeIcon icon={faUserSecret} />{" "}About</Nav.Link></li>
+        <li><Nav.Link href="#About" onClick={handleClose} ><FontAwesomeIcon icon={faUserSecret} />{" "}About</Nav.Link></li>
         <li><Nav.Link href="#Projects" onClick={handleClose}  ><FontAwesomeIcon icon={faFolderOpen} />{" "}Projects</Nav.Link></li>
         <li><Nav.Link href="#Contact" onClick={handleClose} ><FontAwesomeIcon icon={faEnvelopeCircleCheck} />{" "}Contact</Nav.Link></li>
     </ul>
