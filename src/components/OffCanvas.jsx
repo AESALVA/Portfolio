b211defbd8556b3c0180e5639b4ec44933706a4c
AESalva <eduardo_salva@hotmail.com>
2023-03-30 18:45:04 -0300

    offcanvas does not work F

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import {useState}  from "react";
+import { useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import "../styles/navbar.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBarsStaggered, faHouse, faUserSecret, faFolderOpen, faEnvelopeCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBarsStaggered,
+  faHouse,
+  faUserSecret,
+  faFolderOpen,
+  faEnvelopeCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import { OffcanvasTitle } from "react-bootstrap";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
+
 
 const OffCanvas = () => {
   const [show, setShow] = useState(false);
@@ -21,19 +28,48 @@ const OffCanvas = () => {
           icon={faBarsStaggered}
         />
       </div>
-      <Offcanvas className="OffCanvas bg-dark" show={show} placement={'top'} onHide={handleClose} name={'top'}>
+      <Offcanvas
+        className="OffCanvas bg-dark"
+        show={show}
+        placement={"top"}
+        onHide={handleClose}
+        name={"top"}
+      >
         <Offcanvas.Header className="px-3 pb-0" closeButton>
-            <OffcanvasTitle className="d-flex Color"><div className='ContainerImg'><img src='Logo.jpg' alt='logo' ></img></div>A.E.S</OffcanvasTitle>
+          <OffcanvasTitle className="d-flex Color">
+            <div className="ContainerImg">
+              <img src="Logo.jpg" alt="logo"></img>
+            </div>
+            A.E.S
+          </OffcanvasTitle>
         </Offcanvas.Header>
         <Offcanvas.Body className="p-2 d-flex align-items-center pt-0">
-    <ul className="NavListResponsive ">
-        <li><Nav.Link onClick={handleClose}  href="https://aesalva.github.io/portfolio/"><FontAwesomeIcon icon={faHouse} />{" "}Home</Nav.Link></li>
-        <li><Nav.Link href="#About" onClick={handleClose} ><FontAwesomeIcon icon={faUserSecret} />{" "}About</Nav.Link></li>
-        <li><Nav.Link href="#Projects" onClick={handleClose}  ><FontAwesomeIcon icon={faFolderOpen} />{" "}Projects</Nav.Link></li>
-        <li><Nav.Link href="#Contact" onClick={handleClose} ><FontAwesomeIcon icon={faEnvelopeCircleCheck} />{" "}Contact</Nav.Link></li>
-    </ul>
+          <ul className="NavListResponsive ">
+            <li>
+              <Nav.Link
+                onClick={handleClose}
+                href="https://aesalva.github.io/portfolio/"
+              >
+                <FontAwesomeIcon icon={faHouse} /> Home
+              </Nav.Link>
+            </li>
+            <li>
+              <Nav.Link href="#About" onClick={handleClose}>
+                <FontAwesomeIcon icon={faUserSecret} /> About
+              </Nav.Link>
+            </li>
+            <li>
+              <Nav.Link href="#Projects" onClick={handleClose}>
+                <FontAwesomeIcon icon={faFolderOpen} /> Projects
+              </Nav.Link>
+            </li>
+            <li>
+              <Nav.Link href="#Contact" onClick={handleClose}>
+                <FontAwesomeIcon icon={faEnvelopeCircleCheck} /> Contact
+              </Nav.Link>
+            </li>
+          </ul>
         </Offcanvas.Body>
-        
       </Offcanvas>
     </>
   );
